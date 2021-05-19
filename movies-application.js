908562b2e7ec31673c98098aae1e0b0cfa33a6b6
Matthew Walker <matthew.r.walker4@gmail.com>
2021-05-19 11:59:04 -0500

    added functionality to update edit movie forms with data from the selected movie

--- movies-application.js
+++ movies-application.js
@@ -1,3 +1,4 @@
+
 const getMovies = () => {
     fetch('https://auspicious-grizzled-unicorn.glitch.me/movies')
         .then(resp => resp.json())
@@ -7,11 +8,36 @@ const getMovies = () => {
             let htmlStr1 = '';
             for(let movie of movies) {
                 htmlStr += `<h1>${movie.title}</h1><p>by: ${movie.director}</p><img src="${movie.poster}" alt="Movie Poster for ${movie.title}" style="width: 100px">`
-                htmlStr1 += `<option value=‘${movie.id}’>${movie.title}</option>`
+                htmlStr1 += `<option value="${movie.id}">${movie.title}</option>`
 
             }
             $('#movieContainer').html(htmlStr);
             $('#movieEditSelector').append(htmlStr1)
+
+            // Selects movie to edit and populates form values with the movie data
+            $('#movieEditSelector').change(() => {
+                $('#movieEditorInputs').removeClass('hideThis');
+                let selectedVal = $('#movieEditSelector').val();
+                console.log('This is the selected value ' + selectedVal);
+                if (selectedVal === 'default') {
+                    $('#movieEditorInputs').addClass('hideThis');
+                }
+
+                for (let movie of movies) {
+                    if (movie.id == selectedVal) {
+                        $('#editTitle').val(movie.title);
+                        $('#editDirector').val(movie.director);
+                        $('#editYear').val(movie.year);
+                        $('#editGenre').val(movie.genre);
+                        $('#editActors').val(movie.actors);
+                        $('#editPlot').val(movie.plot);
+                        $('#editRating').val(movie.rating);
+                        $('#editPoster').val(movie.poster);
+                    }
+                }
+
+            });
+
         }).then(fade_out);
 };
 function fade_out() {
@@ -54,15 +80,15 @@ $("#addMovie").click((e) => {
         .then(getMovies);
 });
 
-$('#movieEditSelector').change(() => {
-    $('#movieEditorInputs').removeClass('hideThis');
-    let selectedVal = $('#movieEditSelector').val();
-    console.log(selectedVal);
-    if (selectedVal === 'default') {
-        $('#movieEditorInputs').addClass('hideThis');
-    }
-
-});
+// $('#movieEditSelector').change(() => {
+//     $('#movieEditorInputs').removeClass('hideThis');
+//     let selectedVal = $('#movieEditSelector').val();
+//     console.log(selectedVal);
+//     if (selectedVal === 'default') {
+//         $('#movieEditorInputs').addClass('hideThis');
+//     }
+//
+// });
 
 getMovies();
 
